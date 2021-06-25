c5dfef02834403630b614f2d3fa21e8bcf21da57
salman <salman@salman-PC>
2021-06-25 11:54:19 +0530

    code isready to push

---
+++ Java3/src/abcd/Script.java
@@ -0,0 +1,15 @@
+package abcd;
+
+public class Script {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+String s1="abt567jiuy";
+System.out.println(s1.replaceAll("[^a-zA-Z]",""));
+	}
+
+}
