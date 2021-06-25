7b552aca1bec8ce9e6a757a2156a48642234960f
salman <salman@salman-PC>
2021-06-25 12:14:54 +0530

    code is www

--- Java3/src/abcd/Script.java
+++ Java3/src/abcd/Script.java
@@ -11,7 +11,7 @@ public class Script {
 String s1="abt567jiuy";
 System.out.println(s1.replaceAll("[^a-zA-Z]",""));
 System.out.println("data is ready ");
-System.out.println("data is imporant");
+System.out.println("data is imporant1");
 	}
 
 }
